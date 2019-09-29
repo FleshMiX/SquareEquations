0b6b523ce63dbf4cca0e2b0dac9895b1cf3bf585
FleshMiX <31885817+FleshMiX@users.noreply.github.com>
2019-09-29 15:43:18 +0300

    Update square.h

--- square.h
+++ square.h
@@ -1,8 +1,15 @@
 #ifndef SQUARE_H
 #define SQUARE_H
 
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <stdlib.h>
+
 int LinearSolve(float b, float c, float* x1);
 int SparedEquality(float a, float b);
 int SquareSolve(float a, float b, float c, float* x1, float* x2);
+int utestSquareSolve(void);
+void flush_input(void);
 
 #endif
